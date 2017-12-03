ac45a309bdbc84a7abb333aa071a94e88e339b39
snada <stefano.nada@gmail.com>
2017-12-03 22:16:21 +0100

    Also compiling on Mac

--- dist/alure/cmake/FindOpenAL.cmake
+++ dist/alure/cmake/FindOpenAL.cmake
@@ -1,10 +1,15 @@
 GET_FILENAME_COMPONENT(PARENT_BINARY_DIR ${PROJECT_BINARY_DIR} DIRECTORY)
 GET_FILENAME_COMPONENT(PARENT_SOURCE_DIR ${PROJECT_SOURCE_DIR} DIRECTORY)
 
+# Check if a better way exists, and if possible to build and link statically?
 IF(WIN32)
   SET(OPENAL_LIBRARY "${PARENT_BINARY_DIR}/openal/Debug/OpenAL32.lib")
 ELSE()
-  SET(OPENAL_LIBRARY "${PARENT_BINARY_DIR}/openal/libopenal.so")
+  IF(APPLE)
+    SET(OPENAL_LIBRARY "${PARENT_BINARY_DIR}/openal/libopenal.dylib")
+  ELSE()
+    SET(OPENAL_LIBRARY "${PARENT_BINARY_DIR}/openal/libopenal.so")
+  ENDIF()
 ENDIF()
 
 SET(OPENAL_INCLUDE_DIR "${PARENT_SOURCE_DIR}/openal-soft-1.18.2/include/AL")
